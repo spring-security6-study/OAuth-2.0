c4fefd7a58a7b244a1c84f84d7595ea98f95d569
YuyoungRhee <bbclara@naver.com>
2024-12-18 23:03:14 +0900

    feat(config): 기본 oauth2Login 방식 설정
    - oauth2Login(Customizer.withDefaults()를 통해 기본 설정

---
+++ src/main/java/com/example/oauthsession/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.oauthsession.config;
+
+import com.example.oauthsession.service.CustomOAuth2UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((csrf) -> csrf.disable());
+
+        http
+                .formLogin((login) -> login.disable());
+
+        http
+                .httpBasic((basic) -> basic.disable());
+
+        http
+                .oauth2Login(Customizer.withDefaults()); //초기 세팅은 default값으로
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
