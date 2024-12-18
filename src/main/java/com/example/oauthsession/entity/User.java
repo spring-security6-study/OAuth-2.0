045378d834b5854403c0ff556fb5c4bf73bf63ec
YuyoungRhee <bbclara@naver.com>
2024-12-19 02:01:49 +0900

    feat(service): 로그인 된 사용자 정보를 db에 저장

---
+++ src/main/java/com/example/oauthsession/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.oauthsession.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String role;
+}
--- src/main/java/com/example/oauthsession/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/oauthsession/service/CustomOAuth2UserService.java
@@ -1,5 +1,7 @@
 package com.example.oauthsession.service;
 
+import com.example.oauthsession.entity.User;
+import com.example.oauthsession.repository.UserRepository;
 import com.example.oauthsession.service.dto.CustomOAuth2User;
 import com.example.oauthsession.service.dto.GoogleResponse;
 import com.example.oauthsession.service.dto.NaverResponse;
@@ -13,6 +15,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     //DefaultOAuth2UserService OAuth2UserService의 구현체
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -31,9 +39,26 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
-        //추후 작성
+        //db저장
+        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+        User existDate = userRepository.findByUsername(username);
 
-        String role = "ROLE_USER";
+        String role = null;
+        if(existDate == null) { //새로 생성
+            User user = new User();
+
+            user.setUsername(username);
+            user.setEmail(oAuth2Response.getEmail());
+            user.setRole("ROLE_USER");
+
+            userRepository.save(user);
+
+        } else { //데이터 존재 -> update
+            existDate.setEmail(oAuth2Response.getEmail());
+
+            role = existDate.getRole();
+            userRepository.save(existDate);
+        }
 
         return new CustomOAuth2User(oAuth2Response, role);
     }
