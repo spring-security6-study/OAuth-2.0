d056935caa34f003e7ffaa8ca92c77e82b3c911d
imzero238 <nayoung8142@gmail.com>
2024-12-23 16:42:55 +0900

    feat: OAuth2 SecurityFilterChain 생성

---
+++ src/main/java/com/nayoung/oauth2/oauth/config/OAuth2SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.nayoung.oauth2.oauth.config;
+
+import com.nayoung.oauth2.oauth.handler.OAuth2AuthenticationSuccessHandler;
+import com.nayoung.oauth2.oauth.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class OAuth2SecurityConfig {
+
+	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+	private final CustomOAuth2UserService customOAuth2UserService;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		 http
+			 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+				 .requestMatchers(
+					 "/kakao_login_medium_narrow.png",
+					 "/oauth2/**"
+				 ).permitAll()
+				 .anyRequest().authenticated()
+			 )
+			 .oauth2Login(oauth2 -> oauth2
+				 .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService))
+				 .successHandler(oAuth2AuthenticationSuccessHandler)
+			 )
+			 .logout(AbstractHttpConfigurer::disable)
+			 .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+	}
+}
